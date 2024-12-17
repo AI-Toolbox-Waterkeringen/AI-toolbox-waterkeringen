4153b8f04c3998019182b1b0c2d0e1d4ac1c502f
Guy-HKV <75747556+guydup@users.noreply.github.com>
2024-12-17 14:20:08 +0100

    change name trainingdata

--- docs/data.py
+++ docs/data.py
@@ -42,25 +42,25 @@ if __name__ == "__main__":
     container_name = os.environ.get("AZURE_CONTAINER")
 
     # Initialize markdown table
-    digigids_markdown = []
-    digigids_markdown.append("| Naam              |")
-    digigids_markdown.append("| :---------------- |")
+    data_markdown = []
+    data_markdown.append("| Naam              |")
+    data_markdown.append("| :---------------- |")
 
     try:
         # List the blobs in the container
         blob_service_client = BlobServiceClient(account_url=connect_str)
         container_client = blob_service_client.get_container_client(container_name)
         _, folder_entries = list_blobs_hierarchical(container_client, [])
-        digigids_markdown += folder_entries
+        data_markdown += folder_entries
     except Exception:
-        digigids_markdown.append("| Error connecting to storage... |")
+        data_markdown.append("| Error connecting to storage... |")
 
     # Read original file line by line
-    with open("docs/digigids.qmd", "r") as fid:
+    with open("docs/data.qmd", "r") as fid:
         src = string.Template(fid.read())
 
     # Replace placeholder text and write file
-    with open("docs/digigids.qmd", "w") as fid:
-        digigids_markdown = "\n".join(digigids_markdown)
-        dst = src.substitute(placeholder=digigids_markdown)
+    with open("docs/data.qmd", "w") as fid:
+        data_markdown = "\n".join(data_markdown)
+        dst = src.substitute(placeholder=data_markdown)
         fid.write(dst)
